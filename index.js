5b979f54548f453b73dab40156360e7a81d79260
Augusto Queirantes <augusto_queirantes@outlook.com>
2020-08-26 11:24:34 -0300

    Add firebase integration

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('oi');
+const db = firebase.firestore();
+
+db.collection("signatures").get().then((result) => {
+  console.log(result)
+  for (let i in result.docs) {
+    console.log(i.id);
+  }
+});
