a13a60481de8c58c50133bf68c9c11545ef1c7bc
Augusto Queirantes <augusto_queirantes@outlook.com>
2020-08-26 12:41:56 -0300

    Add create and list modules

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-const db = firebase.firestore();
-
-db.collection("signatures").get().then((result) => {
-  console.log(result)
-  for (let i in result.docs) {
-    console.log(i.id);
-  }
+firebase.initializeApp({
+  apiKey: 'API_KEY',
+  authDomain: 'AUTH_DOMAIN',
+  projectId: 'PROJECT_ID'
 });
+
+const firestore = firebase.firestore();
+
+function query() {
+  firestore.collection('signatures')
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        console.log(doc.id, " => ", doc.data().name);
+      });
+    })
+}
+
+function create() {
+  const name = document.querySelector('.signer-name').value;
+  const occupation = document.querySelector('.signer-occupation').value;
+  const createdAt = new Date();
+
+  firestore.collection("signatures").add({
+    name: name,
+    occupation: occupation,
+    created_at: createdAt,
+  })
+    .then(function() {
+      console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+      console.error("Error writing document: ", error);
+    });
+}
