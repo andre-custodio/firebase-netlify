e57c6ec6a35a77ebd346dc55cb5089bdffeec5bd
Andre <andreluizdiascustodio@gmail.com>
2020-08-26 16:55:10 -0300

    update-html-js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 firebase.initializeApp({
-  apiKey: 'API_KEY',
-  authDomain: 'AUTH_DOMAIN',
-  projectId: 'PROJECT_ID'
+  apiKey: 'AIzaSyD0qtFjQNMMclAZzEDu4iysSQP5I61uZPs',
+  authDomain: 'quero-hackday.firebaseapp.com',
+  projectId: 'quero-hackday'
 });
 
 const firestore = firebase.firestore();
 
-function query() {
+function querySignatures() {
   firestore.collection('signatures')
     .get()
     .then(function(querySnapshot) {
@@ -16,20 +16,54 @@ function query() {
     })
 }
 
-function create() {
+function queryApprovedTestimony() {
+  firestore.collection('testimony')
+    .where("approved", "==", true)
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        console.log(doc.id, " => ", doc.data().name, "and", doc.data().testimony, "and", doc.data().email);
+      });
+    })
+}
+
+function createSigner() {
   const name = document.querySelector('.signer-name').value;
   const occupation = document.querySelector('.signer-occupation').value;
+  const email = document.querySelector('.signer-email').value;
   const createdAt = new Date();
 
   firestore.collection("signatures").add({
     name: name,
     occupation: occupation,
-    created_at: createdAt,
+    email: email,
+    created_at: createdAt
   })
     .then(function() {
-      console.log("Document successfully written!");
+      console.log("Signature successfully written!");
     })
     .catch(function(error) {
-      console.error("Error writing document: ", error);
+      console.error("Error writing Signature: ", error);
+    });
+}
+
+function createTestimony() {
+  const name = document.querySelector('.author-name').value;
+  const testimony = document.querySelector('.testimony').value;
+  const email = document.querySelector('.author-email').value;
+  const createdAt = new Date();
+
+  firestore.collection("testimony").add({
+    testimony: testimony,
+    name: name,
+    email: email,
+    approved: false,
+    created_at: createdAt
+  })
+    .then(function() {
+      console.log("Testimony successfully written!");
+    })
+    .catch(function(error) {
+      console.error("Error writing testimony: ", error);
     });
 }
